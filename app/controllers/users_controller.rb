2c855346facbdcfff8f8e43aefbe41fbec4867da
mgmilton <matt.milton@gmail.com>
2018-02-08 16:06:45 -0700

    adds form for error messages

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -9,13 +9,21 @@ class UsersController < ApplicationController
   end
 
   def create
-    @user = User.create(user_params)
-    redirect_to user_path(@user)
+    @user = User.new(user_params)
+    if @user.save
+      flash[:notice] = "You signed up successfully"
+      flash[:color] = "valid"
+      redirect_to user_path(@user)
+    else
+      flash[:notice] = "Form is invalid"
+      flash[:color] = "invalid"
+      render :new
+    end
   end
 
   private
 
   def user_params
-    params.require(:user).permit(:name, :email, :password)
+    params.require(:user).permit(:name, :email, :password, :password_confirmation)
   end
 end
