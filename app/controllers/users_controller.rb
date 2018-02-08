56a36d25acfb823db9853d28c285ae001cf42ae3
mgmilton <matt.milton@gmail.com>
2018-02-08 15:34:05 -0700

    adds new page and form

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,16 @@
-class UsersController < AcpplicationController
+class UsersController < ApplicationController
 
+  def new
+    @user = User.new
+  end
 
+  def create
+    @user = User.create(user_params)
+  end
+
+  private
+
+  def user_params
+    params.require(:user).permit(:name, :email, :password)
+  end
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,3 +1,4 @@
 class User < ApplicationRecord
-  validates_presence_of :name, :email, :password
+  validates_presence_of :name, :password
+  validates :email, presence: true, uniqueness: true
 end
